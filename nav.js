fe2b63d405e24c659e4cd74c9e20ff0221250ad2
kustomduo <kustomduo@gmail.com>
2026-08-24 17:39:07 +0900

    새 레트로 로고를 공통 헤더에 적용

--- nav.js
+++ nav.js
@@ -8,7 +8,7 @@
   var SUPABASE_ANON_KEY = 'sb_publishable_5GA_EH7mqRbkWe-UEWEL2Q_xf5cn3kF';
   var header =
     '<header class="site-header"><div class="inner">' +
-      '<a class="brand" href="index.html"><span class="brand-mark"><img src="thundermiri-icon-192.png" alt="" /></span><span class="brand-full">동탄이네 썬더미리</span><span class="brand-short">동탄이네 썬더미리</span></a>' +
+      '<a class="brand" href="index.html" aria-label="동탄이네 썬더미리 홈"><span class="brand-logo"><img src="thundermiri-retro-logo.png" alt="동탄이네 썬더미리" /></span></a>' +
       '<button type="button" class="menu-toggle" aria-expanded="false" aria-controls="siteMenu">메뉴</button>' +
       '<nav class="nav" id="siteMenu" aria-label="주요 메뉴">' +
         '<a href="index.html#radar">우리 동네 레이더</a>' +
